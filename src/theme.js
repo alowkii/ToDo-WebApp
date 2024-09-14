e6440e1c6be96d281bc1cdb6ed13e9144754acd7
alowkii <paalok333@gmail.com>
2024-09-14 23:46:08 +0545

    It is possible to change theme now.

--- src/theme.js
+++ src/theme.js
@@ -14,6 +14,25 @@ function themeChange(){
             icon.classList.add('fa-sun');
         }
     }, 250);
+
+    const root = document.querySelector(':root');
+    const currentTheme = root.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+
+    root.setAttribute('data-theme', newTheme);
+
+    changeTheme(newTheme);
 }
+
+function changeTheme(theme) {
+    const root = document.querySelector(':root');
+
+    root.style.setProperty('--primary-color', `var(--primary-${theme}-color)`);
+    root.style.setProperty('--secondary-color', `var(--secondary-${theme}-color)`);
+    root.style.setProperty('--tertiary-color', `var(--tertiary-${theme}-color)`);
+    root.style.setProperty('--quaternary-color', `var(--quaternary-${theme}-color)`);
+    root.style.setProperty('--quinary-color', `var(--quinary-${theme}-color)`);
+}
+
 
 window.themeChange = themeChange;
