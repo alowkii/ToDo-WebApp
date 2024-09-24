684288bcc84379a0149c9493d93792826df69935
alowkii <paalok333@gmail.com>
2024-09-24 22:16:38 +0530

    Deleted localStorage.js file for now. Will add it back later.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import "./styles.css";
 import "./icon.js";
 import "./theme.js";
 import "./menu.js";
-import {populateStorage} from "./localStorage.js";
 import { toggleNotificationWindow, addNotification } from "./notification-window.js";
 
 setTheme(localStorage.getItem("theme"));
