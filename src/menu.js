e140dbbf1e2dac56f3ffbbf10f5fd5d205963401
alowkii <paalok333@gmail.com>
2024-09-29 22:07:23 +0530

    Removed the automatic slide off for the menu and removed the menu close button and adjoined the feature into the same button that opens the window.

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,8 @@ export {toggleMenu};
 
 function toggleMenu(){
     createMenu();
-    const menu = document.querySelector('.menu-window');
+
+    const menu = document.querySelector('#menu-window');
     if(menu.style.display == 'block'){
         hideMenu();
     }else{
@@ -11,37 +12,31 @@ function toggleMenu(){
 }
 
 function hideMenu(){
-    const menu = document.querySelector('.menu-window');
+    const menu = document.querySelector('#menu-window');
     menu.style.animation = 'slideOut 0.5s forwards';
+    const menuBtn = document.getElementById('menuBtn');
+    menuBtn.style.transform = 'rotate(0deg)';
+    menuBtn.style.transition = '0.5s';
     setTimeout(() => {
         menu.style.display = 'none';
     }, 500);
 }
 
 function showMenu(){
-    const menu = document.querySelector('.menu-window');
+    const menu = document.querySelector('#menu-window');
     menu.style.animation = 'slideIn 0.5s forwards';
     menu.style.display = 'block';
+    const menuBtn = document.getElementById('menuBtn');
+    menuBtn.style.transform = 'rotate(90deg)';
+    menuBtn.style.transition = '0.5s';
 }
 
 function createMenu() {
     const menu = document.createElement('div');
-    menu.classList.add('menu-window');
+    menu.id ='menu-window';
     menu.style.display = 'none';
-
-    const menuBtn = document.getElementById('menuBtn');
-    const menuBtnInMenu = menuBtn.cloneNode(true);
-    menu.appendChild(menuBtnInMenu);
     
     document.querySelector('menu').appendChild(menu);
-
-    document.addEventListener('click', (e) => {
-        if(!menu.contains(e.target) && e.target != menu){
-            hideMenu();
-        }else{
-            showMenu();
-        }
-    });
 }
 
 window.toggleMenu = toggleMenu;
